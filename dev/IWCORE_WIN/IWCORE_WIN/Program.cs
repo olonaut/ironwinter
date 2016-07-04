c5cf1d3f1d131fa3bb844718bfc85289c9a50ac0
Marcel Kurz <realmarra@gmail.com>
2016-07-04 19:40:12 +0200

    + Leere Klassen

--- dev/IWCORE_WIN/IWCORE_WIN/Program.cs
+++ dev/IWCORE_WIN/IWCORE_WIN/Program.cs
@@ -14,7 +14,7 @@ namespace IWCORE_WIN
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new Core())
                 game.Run();
         }
     }
